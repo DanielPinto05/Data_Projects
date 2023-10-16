2bcf4f2708a1e3cec34e1251750584043b191346
Daniel G. Pinto <danielpintolzr@gmail.com>
2023-10-16 11:58:28 -0400

    Created matrixmult.py for LinAlg class

---
+++ matrixmult.py
@@ -0,0 +1,9 @@
+# multiplies Matrix A (3x3) with Vector x (3x1) and stores the result as vector b
+matrixA = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+vectorX = [-3, 4, 1]
+
+
+def matrix_times_vector(matrix, vector):
+    # I want to try to this myself. Thanks CoPilo!
+    pass
